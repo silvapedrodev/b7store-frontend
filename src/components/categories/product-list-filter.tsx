9ca77a1ef49017748c05258f9b955ad72d5d9f34
Pedro Silva <150725974+silvapedrodev@users.noreply.github.com>
2026-05-29 13:56:54 -0300

    feat(products): add product listing to screen

--- src/components/categories/product-list-filter.tsx
+++ src/components/categories/product-list-filter.tsx
@@ -3,6 +3,8 @@
 import { UseQueryString } from "@/hooks/use-querystring";
 import { ChangeEvent, useState } from "react";
 import { FilterGroup } from "./filter-group";
+import { data } from "@/data";
+import { ProductItem } from "../product-item";
 
 export const ProductListFilter = () => {
   const queryString = UseQueryString();
@@ -17,7 +19,10 @@ export const ProductListFilter = () => {
   return (
     <div>
       <div className="flex flex-col md:flex-row gap-5 justify-between items-start md:items-center">
-        <div className="text-2xl md:text-3xl"><strong>99</strong> Produtos</div>
+        <div className="text-2xl md:text-3xl">
+          <strong>{data.products.length} </strong>
+          Produto{ data.products.length !== 1 ? 's' : ''}
+        </div>
         <div className="w-full md:max-w-70 flex flex-row gap-5">
           <select
             defaultValue={order}
@@ -42,11 +47,10 @@ export const ProductListFilter = () => {
           <FilterGroup id="tech" name="Tecnologias" />
           <FilterGroup id="color" name="Cores" />
         </div>
-        <div className="flex-1 grid grid-cols-1 md:grid-cols-3">
-          <div>...</div>
-          <div>...</div>
-          <div>...</div>
-          <div>...</div>
+        <div className="flex-1 grid grid-cols-1 md:grid-cols-3 gap-8">
+          {data.products.map(item => (
+            <ProductItem key={item.id} data={item} />
+          ))}
         </div>
       </div>
     </div>
