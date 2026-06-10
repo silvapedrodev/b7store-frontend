359230d6fb8fb300230acb9237cd4695856cfac7
Pedro Silva <150725974+silvapedrodev@users.noreply.github.com>
2026-06-10 10:21:05 -0300

    feat(addresses): integrate address management API

--- src/actions/add-user-addres.ts
+++ src/actions/add-user-addres.ts
@@ -1,9 +1,19 @@
 "use server"
 
-import { data } from "@/data"
+import { api } from "@/libs/axios"
 import { Address } from "@/types/address"
+import { getUserAddresses } from "./get-user-addresses";
 
 export const addUserAddress = async (token: string, address: Address) => {
-  // To-do: Requisição para adicionar novo endereço
-  return data.addresses;
+  try {
+    const response = await api.post('user/addresses', { ...address }, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    if (response.status === 201) {
+      return getUserAddresses(token);
+    }
+  } catch { }
+  return [];
 }
--- src/actions/get-user-addresses.ts
+++ src/actions/get-user-addresses.ts
@@ -1,10 +1,18 @@
 "use server"
 
-import { data } from "@/data"
+import { api } from "@/libs/axios";
 import { Address } from "@/types/address";
 
 export const getUserAddresses = async (token: string): Promise<Address[]> => {
-  // To-do pegar endereços do usuário pelo token.
-
-  return data.addresses;
+  try {
+    const response = await api.get('/user/addresses', {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    if (response.status === 200) {
+      return response.data.addresses as Address[];
+    }
+  } catch { }
+  return [];
 }
